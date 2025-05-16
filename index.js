33691d2d8013464a5d4218605e603dc4e66d936b
mohibbullah272 <bullahmohi50@gmail.com>
2025-05-16 22:25:50 +0600

    add navbar and banner section

---
+++ index.js
@@ -0,0 +1,35 @@
+let isMenuOpen=false
+function handleMenuBar(){
+const menuOptions= document.getElementById('menu_container')
+const menuIcon = document.getElementById('menu_icon')
+
+isMenuOpen=!isMenuOpen
+
+if(isMenuOpen){
+    menuOptions.classList.remove('hidden')
+
+menuOptions.innerHTML=`
+<ul  style="list-style: none; gap:10px;">
+<li>Home</li>
+<li>Services </li>
+<li>Portfolio</li>
+<li>Reviews</li>
+<li>Pricing</li>
+<li>White Level</li>
+</ul>
+
+`
+document.getElementById('menu').innerHTML=`
+<img id="menu_icon" style="width: 25px;" src="./assets/icons8-cancel-50.png" alt="menu_icon">
+
+
+`
+menuIcon.classList.add('hidden')
+}else{
+    menuOptions.classList.add('hidden')
+    document.getElementById('menu').innerHTML=`
+    <img id="menu_icon" style="width: 25px;" src="./assets/icons8-menu-50.png" alt="menu_icon">`
+
+}
+
+}
